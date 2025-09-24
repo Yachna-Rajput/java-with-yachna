7854b94cc4a07554375559d3a4b11ada3e1b6eba
Yachna Rajput <yachnarajput7890@gmail.com>
2025-09-24 08:48:00 -0700

    update

--- src/b_arrays/ArrayUtility.java
+++ src/b_arrays/ArrayUtility.java
@@ -1,4 +1,20 @@
 package b_arrays;
 
+import java.util.Scanner;
+
 public class ArrayUtility {
+
+    public static int[] inputArray() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array : ");
+        int size = sc.nextInt();
+
+        int[] array = new int[size];
+
+        for (int i = 0; i <= size; i++) {
+            System.out.println("Element " + i + " :");
+            array[i] = sc.nextInt();
+        }
+        return array;
+    }
 }
