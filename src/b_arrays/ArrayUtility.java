e8611ca6ec9c2c76e5b0297c0bb80453727ad91d
Yachna Rajput <yachnarajput7890@gmail.com>
2025-10-23 08:34:13 -0700

    add 2D array method

--- src/b_arrays/ArrayUtility.java
+++ src/b_arrays/ArrayUtility.java
@@ -11,10 +11,35 @@ public class ArrayUtility {
 
         int[] array = new int[size];
 
-        for (int i = 0; i <= size; i++) {
-            System.out.println("Element " + i + " :");
+        for (int i = 0 ; i < size; i++) {
+            System.out.print("Element " + (i+1) + " :");
             array[i] = sc.nextInt();
         }
         return array;
+    }
+
+    public static int[][] input2DArray() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number of Rows : ");
+        int rows = sc.nextInt();
+        System.out.print("Enter the Number of Columns :");
+        int columns = sc.nextInt();
+
+        int[][] array = new int[rows][columns];
+
+        for (int i = 0 ; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print("Element of rows " + (i + 1) + " and column " + (j + 1) + " is : ");
+                array[i][j] = sc.nextInt();
+            }
+        }
+        return array;
+    }
+
+    public static void displayArray(int[] array ){
+        for(int i =0; i< array.length; i++){
+            System.out.print(array[i]+ " ");
+        }
+        System.out.println();
     }
 }
